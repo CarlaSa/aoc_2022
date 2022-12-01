bc5df36727f1e6554d78d8b490eef19034439921
Carla Sagebiel <carlachristin@googlemail.com>
2022-12-01 07:57:42 +0100

    day 1

--- 01.py
+++ 01.py
@@ -1,9 +1,9 @@
-import aocd
-from aocd import get_data
+from aocd import get_data, submit
 
 data = get_data(year=2022, day=1)
 cals = [sum(int(i) for i in d.split("\n")) for d in data.split("\n\n")]
 sol1 = max(cals)
 sol2 = sum(sorted(cals)[-3:])
 
-aocd.submit(sol2)
+# submit(sol1)
+submit(sol2)
