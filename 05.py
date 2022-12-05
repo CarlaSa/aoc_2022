4b0a0bdb2352fc4840c1924404af139dbf95b220
Carla Sagebiel <carlachristin@googlemail.com>
2022-12-05 12:40:03 +0100

    day 5

--- 05.py
+++ 05.py
@@ -1,9 +1,12 @@
 from aocd import get_data, submit
+import pandas as pd
+from io import StringIO
+from copy import deepcopy
 
 data = get_data(year=2022, day=5)
 crates, instructions = data.split("\n\n")
 
-# print(crates)
+# first idea
 lines = [[x[i:i + 4] for i in range(0, len(x), 4)] for x in crates.split("\n")]
 lines = [[lines[j][i].strip() for j in range(len(lines[0]) - 1, -1, -1)]
          for i in range(0, len(lines))]
@@ -11,31 +14,48 @@ lines_dict = {
     int(ll[0]): [ll[i][1] for i in range(1, len(ll)) if len(ll[i]) > 0] for ll in lines
 }
 
+# second method using pandas
+d = pd.read_fwf(StringIO(crates), header = None)
+d = d.rename(columns=d.iloc[8]).drop(index = 8).reset_index(drop=True) # last row to column
+d = d.applymap(lambda x: x.strip("[] "), na_action = "ignore") # remove brackets
+d = d.reindex(index=d.index[::-1]) # turn around
+lines_dict_alt = {int(k): [y for y in d[k].to_list() if not pd.isna(y)] for k in d.columns}
+
 
 def get_upper_crates(l_d):
+    """get the correct output from moved crates (the uppermost crates from left to right)"""
     upper = [l_d[i][-1] for i in range(1, 10)]
     return "".join(upper)
 
 
-def move(number, from_, to_):
-    for _ in range(number):
-        lines_dict[to_].append(lines_dict[from_].pop())
+def task1(l_dict):
+    """task 1"""
+    def move(number, from_, to_):
+        for _ in range(number):
+            l_dict[to_].append(l_dict[from_].pop())
+    for line in instructions.split("\n"):
+        l = line.split()
+        move(int(l[1]), int(l[3]), int(l[5]))
+    return l_dict
 
-def move2(number, from_, to_):
-    temp = []
-    for _ in range(number):
-        temp.insert(0, (lines_dict[from_].pop()))
-    lines_dict[to_] += temp
 
-for line in instructions.split("\n"):
-    l = line.split()
-    move2(int(l[1]), int(l[3]), int(l[5]))
+def task2(l_dict):
+    """task 2"""
+    def move(number, from_, to_):
+        temp = []
+        for _ in range(number):
+            temp.insert(0, (l_dict[from_].pop()))
+        l_dict[to_] += temp
+    for line in instructions.split("\n"):
+        l = line.split()
+        move(int(l[1]), int(l[3]), int(l[5]))
+    return l_dict
 
-#after
-print(get_upper_crates(lines_dict))
+sol1 = get_upper_crates(task1(deepcopy(lines_dict)))
+sol2 = get_upper_crates(task2(deepcopy(lines_dict)))
 
-# print(instructions)
-submit(get_upper_crates(lines_dict))
+print(sol1)
+print(sol2)
 
-# with move is sol1
-# with move2 is sol2
+#submit(sol1)
+#submit(sol2
