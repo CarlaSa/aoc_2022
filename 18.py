4bfa9692f2c20a168bb9ef26b577d4294da19157
Carla Sagebiel <carlachristin@googlemail.com>
2022-12-18 10:39:04 +0100

    unfinished

--- 18.py
+++ 18.py
@@ -1,20 +1,19 @@
 from aocd import get_data, submit
-from collections import deque
+from collections import deque, defaultdict
 
 use_test = False
 data = get_data(year=2022, day=18)
-with open("input.txt") as f:
-    data, use_test = f.read(), True
+# with open("input.txt") as f:
+#     data, use_test = f.read(), True
 
-
-data_set = set(data.split("\n")) # for lookup
-data = [[int(x) for x in d.split(",")]for d in data.split("\n")]
+data_set = set(data.split("\n"))  # for lookup
+data = [[int(x) for x in d.split(",")] for d in data.split("\n")]
 neighbors = set()
 
 surface = 0
 for d in data:
     sides = 6
-    for i in [[-1, 0, 0], [1,0,0], [0,-1,0], [0, 1, 0], [0,0,1], [0,0,-1]]:
+    for i in [[-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, 1], [0, 0, -1]]:
         n = [d[j] + i[j] for j in range(3)]
         n_str = ",".join(str(nn) for nn in n)
         if n_str in data_set:
@@ -24,28 +23,25 @@ for d in data:
 
     surface += sides
 
-#submit(surface)
+submit(surface, part="a")
 print("Task 1: ", surface)
 
 data_max_i = [max(d[i] for d in data) for i in range(3)]
 data_min_i = [min(d[i] for d in data) for i in range(3)]
-# data_max = max(d for dd in data for d in dd)
-# data_min = min(d for dd in data for d in dd)
 def definitly_outside(x):
     return not all(data_min_i[i] - 1 < x[i] < data_max_i[i] + 1 for i in range(3))
-    #return not all( data_min < xx < data_max for xx in x)
+
 
 # find interior surface
-looked_at = {n:False for n in neighbors}
+# looked_at = {n: False for n in neighbors}
+looked_at = defaultdict(lambda: False)
+outside = set()
 
-neighbor_list = list(neighbors)
-for n in neighbor_list:
+for n in neighbors:
     if looked_at[n]:
         continue
     trapped_surface = 0
-    is_trapped = True
     q = deque([n])
-    unknown = 0
     while len(q) > 0:
         curr = q.popleft()
         if looked_at[curr]:
@@ -57,34 +53,92 @@ for n in neighbor_list:
             n_n = [int(curr_list[j]) + i[j] for j in range(3)]
             n_n_str = ",".join(str(nn) for nn in n_n)
             if n_n_str in data_set:
-                trapped_surface +=1
-            elif n_n_str in neighbors:
-                if not looked_at[n_n_str]:
-                    q.append(n_n_str)
+                trapped_surface += 1
+            elif n_n_str in outside:
+                outside.add(curr)
+                looked_at[n_n_str] = True
+                trapped_surface = 0
+                # q = []
+                # break
             else:
-                if n_n_str in looked_at:
+                if looked_at[n_n_str]:
                     continue
                 else:
-                    looked_at[n_n_str] = False
                     if definitly_outside(n_n):
+                        outside.add(n_n_str)
                         looked_at[n_n_str] = True
-                        is_trapped = False
+                        trapped_surface = 0
                         q = []
                         break
                     else:
-                        unknown += 1
                         q.append(n_n_str)
-                # is_trapped = False
-                # q = []
-                # break
-        # if unknown > 45:
-        #     is_trapped = False
-        #     q = []
-        #     break
-    if is_trapped:
-        surface -= trapped_surface
+    surface -= trapped_surface
 
 print("Task 2: ", surface)
 # if not use_test:
 #     if surface > 0:
 #         submit(surface)
+
+
+# for n in neighbors:
+#     if looked_at[n]:
+#         continue
+#     trapped_surface = 0
+#     is_trapped = True
+#     q = deque([n])
+#     unknown = 0
+#     while len(q) > 0:
+#         curr = q.popleft()
+#         if looked_at[curr]:
+#             continue
+#         else:
+#             looked_at[curr] = True
+#         curr_list = curr.split(",")
+#         for i in [[-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, 1], [0, 0, -1]]:
+#             n_n = [int(curr_list[j]) + i[j] for j in range(3)]
+#             n_n_str = ",".join(str(nn) for nn in n_n)
+#             if n_n_str in data_set:
+#                 trapped_surface += 1
+#
+#             elif n_n_str in outside:
+#                 outside.add(curr)
+#                 looked_at[n_n_str] = True
+#                 is_trapped = False
+#                 q = []
+#                 break
+#             # elif n_n_str in neighbors:
+#             #     if not looked_at[n_n_str]:
+#             #         q.append(n_n_str)
+#             else:
+#                 if looked_at[n_n_str]:
+#                     continue
+#                 else:
+#                     if definitly_outside(n_n):
+#                         outside.add(n_n_str)
+#                         looked_at[n_n_str] = True
+#                         is_trapped = False
+#                         q = []
+#                         break
+#                     else:
+#                         q.append(n_n_str)
+#                 # if n_n_str in looked_at:
+#                 #     continue
+#                 # else:
+#                 #     looked_at[n_n_str] = False
+#                 #     if definitly_outside(n_n):
+#                 #         looked_at[n_n_str] = True
+#                 #         is_trapped = False
+#                 #         q = []
+#                 #         break
+#                 #     else:
+#                 #         unknown += 1
+#                 #         q.append(n_n_str)
+#                 # is_trapped = False
+#                 # q = []
+#                 # break
+#         # if unknown > 45:
+#         #     is_trapped = False
+#         #     q = []
+#         #     break
+#     if is_trapped:
+#         surface -= trapped_surface
